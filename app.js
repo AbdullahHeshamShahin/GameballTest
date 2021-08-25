9d58c58bcdea7a2d8d96fae38934ce9851b76620
AbdullahHeshamShahin <64377604+AbdullahHeshamShahin@users.noreply.github.com>
2021-08-25 22:09:08 +0200

    v
    v

--- app.js
+++ app.js
@@ -63,6 +63,5 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); }); 
 
 module.exports = app;
