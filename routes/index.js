32b3d5013141239fbb65f2215b22c468e813daee
AbdullahHeshamShahin <64377604+AbdullahHeshamShahin@users.noreply.github.com>
2021-08-26 18:41:59 +0200

    Update index.js
    v

--- routes/index.js
+++ routes/index.js
@@ -120,5 +120,49 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+var appRouter = function(app) {
 
+	app.get('/', function(req, res) {
+    	return res.send('Welcome to Intuit Webhooks Sample App');
+	});
+
+	 /**
+     * Method to receive webhooks event notification 
+     * 1. Validates payload
+     * 2. Adds it to a queue
+     * 3. Sends success response back
+     * 
+     * Note: Queue processing happens asynchronously
+     */
+
+    router.post('/', function(req, res) {
+
+		var payload = JSON.stringify(req.body);
+		var signature = req.get('XXii5DLKG-sFoxbR2qhnSw')
+
+		// if signature is empty return 401
+		if (!signature) {
+			return res.status(401).send('FORBIDDEN');
+		}
+
+		// if payload is empty, don't do anything
+		if (!payload) {
+			return res.status(200).send('success');
+		}
+		
+		// validate signature
+		if (util.isValidPayload(signature, payload)) {
+
+			// add to queue
+			queue.addToQueue(payload);
+			console.log('task added to queue ');
+		
+			return res.status(200).send('success');
+		} else {
+			return res.status(401).send('FORBIDDEN');
+		}
+
+	});
+
+}
 module.exports = router;
